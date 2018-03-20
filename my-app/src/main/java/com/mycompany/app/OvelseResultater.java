c4b8cf0d116476d27e5072423d3340e92a646966
trymhog97 <tehogeli@stud.ntnu.no>
2018-03-20 16:01:29 +0100

    Arbeid med showResults-metoden

--- my-app/src/main/java/com/mycompany/app/OvelseResultater.java
+++ my-app/src/main/java/com/mycompany/app/OvelseResultater.java
@@ -3,10 +3,12 @@ package com.mycompany.app;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.Scanner;
 import java.util.Date;
 import java.util.ArrayList;
 import java.sql.ResultSet;
+import java.util.Calendar;
 
 import com.mycompany.app.Connect;
 
@@ -24,7 +26,7 @@ public class OvelseResultater {
 			ResultSet rs = stmt.executeQuery(sql);
 			while (rs.next()) {
 				tidspunkt.add(rs.getDate("Dato"));
-				//System.out.println(rs.getDate("Dato"));
+				System.out.println(rs.getDate("Dato"));
 			}
 			return tidspunkt;
 		}catch (SQLException e) {
@@ -34,8 +36,35 @@ public class OvelseResultater {
 	}
 	return null;
 }
+	
+	
+	public static ArrayList<String> showResults(String ovelse, Date startDate, Date endDate){
+		ArrayList<String> ovelser = new ArrayList<String>();
+		ArrayList<Date> datoer = getOvelseTidspunkt();
+		try {
+			stmt = conn.createStatement();
+			String sql = "SELECT * FROM Ovelse";
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				ovelser.add(rs.getString("Ovelsenavn"));
+				System.out.println(ovelser);
+				}
+			if(ovelser.contains(ovelse)) {
+				
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
 	public static void main(String[] args) {
-		//ArrayList<Date> tidspunkt = new ArrayList<Date>();
-		System.out.println(getOvelseTidspunkt());
+		ArrayList<Date> tidspunkt = getOvelseTidspunkt();
+		 Date date1 = tidspunkt.get(0);
+		 Date date2 = tidspunkt.get(1);
+		 System.out.println(date2);
 	}
 }
