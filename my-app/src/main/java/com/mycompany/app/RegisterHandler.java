5cd41b646f4012c2f1d9b2ea921dc7756bbdefd0
svenngronbeck <srgronbe@stud.ntnu.no>
2018-03-20 16:18:26 +0100

    Lagt til notater og ovelser i treningsøkt, og tror krav 2 er oppfylt nå

--- my-app/src/main/java/com/mycompany/app/RegisterHandler.java
+++ my-app/src/main/java/com/mycompany/app/RegisterHandler.java
@@ -6,7 +6,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Scanner;
 import com.mycompany.app.Connect;
 
@@ -54,11 +56,17 @@ public class RegisterHandler {
 	}
 
 	//Legge til en treningsøkt
-	public static void registerTreningsokt(int oktid, Date dato, int varighet, String form, String prestasjon) {
+	public static void registerTreningsokt(int oktid, Date dato, int varighet, int form, int prestasjon, ArrayList<String> ovelser, String notat) {
 		try {
 			stmt = conn.createStatement();
 			String sql = String.format("INSERT INTO `Treningsokt`(`OktID`, `Dato`, `Varighet`, `Form`, `Prestasjon`) VALUES ('%s', '%s', '%s', '%s', '%s' )", oktid, dato, varighet, form, prestasjon);
+			String sql_notat = String.format("INSERT INTO `NotatForOkt`(`OktID`, `Beskrivelse`) VALUES ('%s', '%s')", oktid, notat);
 			stmt.executeUpdate(sql);
+			stmt.executeUpdate(sql_notat);
+			for(int x=0; x < ovelser.size(); x++) {
+				String sql1 = String.format("INSERT INTO `OvelseForOkt`(`Ovelsenavn`, `OktID`) VALUES ('%s', '%s')", ovelser.get(x), oktid);
+				stmt.executeUpdate(sql1);
+				}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
@@ -71,6 +79,8 @@ public class RegisterHandler {
 		try {
 			stmt = conn.createStatement();
 			String sql = "SELECT * FROM Treningsokt order by OktID DESC";
+			Statement stmt2 = conn.createStatement();
+			
 			ResultSet result = stmt.executeQuery(sql);
 			while(result.next()) {
 				int oktID = result.getInt("OktID");
@@ -79,7 +89,21 @@ public class RegisterHandler {
 				int varighet = result.getInt("Varighet");
 				String form = result.getString("Form");
 				String prestasjon = result.getString("Prestasjon");
-				System.out.println("Okt nr " + String.valueOf(oktID) + ": " + '\n' + "Dato: " + '\n' + String.valueOf(dato) + "Tidspunkt: " + String.valueOf(tidspunkt) + '\n' + "Varighet: " + String.valueOf(varighet) + '\n' + "Form: " + form + '\n' + "Prestasjon: " + prestasjon + '\n' + "----------------------------" + '\n' + '\n');
+				
+				String sql_notater = String.format("SELECT `Beskrivelse` FROM `NotatForOkt` WHERE `OktID`='%s'", oktID);
+				
+				ResultSet result_notat = stmt2.executeQuery(sql_notater);
+				while(result_notat.next()) {
+					//System.out.println(result_notat);
+					String notat = result_notat.getString("Beskrivelse");
+					System.out.println("Okt nr " + String.valueOf(oktID) + ": " + '\n' + "Dato: " + '\n' + String.valueOf(dato) + 
+						"\nTidspunkt: " + String.valueOf(tidspunkt) + '\n' + "Varighet: " + String.valueOf(varighet) + '\n' + 
+						"Form: " + form + '\n' + "Prestasjon: " + prestasjon + '\n' + 
+						"Notat: " + notat + '\n' +  "----------------------------" + '\n' + '\n'
+						);
+				}
+				
+				
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -89,9 +113,12 @@ public class RegisterHandler {
 	}
 	
 	public static void main(String[] args) {
-		/*java.sql.Date currentDate = new java.sql.Date(Calendar.getInstance().getTime().getTime());
-		registerTreningsokt(1, currentDate, 50, "Grei form", "Greit prestert");
-		registerTreningsokt(2, currentDate, 70, "Sykt bra form", "Sykt bra prestert");*/ 
+		//java.sql.Date currentDate = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+		//registerTreningsokt(1, currentDate, 50, "Grei form", "Greit prestert");
+		//registerTreningsokt(2, currentDate, 70, "Sykt bra form", "Sykt bra prestert");*/ 
+		//ArrayList<String> testListe = new ArrayList<String>();
+		//testListe.add("TestØvels1");
+		//registerTreningsokt(1, currentDate, 40, 10, 8, testListe, "Kort og god økt.");
 		infoOmSisteOkter(5);
 		
 	}
